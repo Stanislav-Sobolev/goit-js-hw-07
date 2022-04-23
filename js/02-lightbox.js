0a5ac6a24d700c727d5210dd1e59fa83e5523c63
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-04-23 19:51:17 +0300

    done-1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector("ul.gallery");
+
+const previewGalleryList = galleryItems.map( ({preview, original, description}) => {
+    
+ return `<li><a class="gallery__item" href="${original}">
+ <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"/>
+</a></li>`
+}).join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", previewGalleryList);
+
+galleryContainer.addEventListener("click", clickToImg);
+
+function clickToImg (event) {
+    event.preventDefault()
+  if(event.target.nodeName !== "IMG") {
+    return;
+  }
+};
+
+const lightbox = new SimpleLightbox('.gallery__item', 
+{captionsData: "alt", captionDelay: 250,});
